ee56d4c1cfa930660ec30f0ae1806dc07c8b99e7
Marco <mmondini@mondspace.com>
2019-10-22 00:21:24 +0200

    ✨🛠🐳

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 let j = schedule.scheduleJob('45 * * * * *', () => {
   console.log('The answer to life, the universe, and everything!');
 
-  fs.writeFile('./test', 'Hey hey' + new Date(), (err) => {
+  fs.writeFile('./test', new Date(), (err) => {
     if (err) {
       return console.error(err);
     }
-    console.log('Written');
+    console.log('Written at /app/test inside the container');
   });
 
 });
